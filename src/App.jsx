a1fb6eca0b6c19ad8a83027d54494732009268ae
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-07-25 15:04:14 +0300

    created navbar component file w/catherine kiiru
    Signed-off-by: Catherine <66266208+CatherineKiiru@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1>Github Copilot Hackathon Project</h1>;
+  return ( 
+  <Navbar/>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Navbar() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Navbar

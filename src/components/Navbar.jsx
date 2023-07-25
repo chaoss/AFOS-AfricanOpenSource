494693211407de9ac4386a2b29de5b6aebccc7c0
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-07-25 18:46:12 +0300

    complete Navbar component w/catherine
    Signed-off-by: Catherine <66266208+CatherineKiiru@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/afos-logo.svg";
 
 function Navbar() {
   return (
-    <div className=''>
-        <h1>afos</h1>
+    <div className="w-[396px] md:w-full py-6 px-16">
+      <div className="flex justify-between">
+        <div className="">
+          <img src={logo} alt="logo" className="w-20 h-10" />
+        </div>
+        <div className="text-green text-base">
+          <a
+            href="#"
+            className="px-4 py-2 mr-4"
+          >
+            About Afos
+          </a>
+          <a
+            href="#"
+            className="px-4 py-2"
+          >
+            Projects
+          </a>
+          <a
+            href="#"
+            className="px-4 py-2"
+          >
+            Community
+          </a>
+          <button className="bg-yellow rounded-md px-9 py-4 font-semibold">
+            Submit Project
+          </button>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    // screens:{
+    //   'xs': '375px',
+    //   'sm': '640px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    // },
+    extend: {
+      colors: {
+        green: "hsla(60, 100%, 10%, 1)",
+        black: "hsla(0, 0%, 0%, 1)",
+        grey: "hsla(hsla(0, 0%, 22%, 1)",
+        yellow: "hsla(46, 96%, 65%, 1)",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};

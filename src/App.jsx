933ded1fdc7b5786b866c0f57d1aad77dbc5e960
Shubhankar <shubhankar.nautiyal21@gmail.com>
2023-08-02 15:04:20 +0530

    Added Empowerment section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Empowerment from "./components/Empowerment";
 import Enhancement from "./components/Enhancement";
 
 function App() {
     return (
         <div>
             <h1>Github Copilot Hackathon Project</h1>
+            <Empowerment />
             <Enhancement />
         </div>
     );

d86e28b0ac0fd64ee9d4d9830d00e30a364eba17
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-08-08 13:18:12 +0300

    created responsive navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,48 +3,58 @@ import React, { useState } from "react";
 import logo from "../assets/images/afos-logo.svg";
 
 function Navbar() {
-  const [NavOpen, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
   return (
-    <div className="flex justify-between md:w-full p-5 md:px-32 text-green">
+    <div className="flex justify-between md:w-full p-5 md:px-10 text-green">
       <img src={logo} alt="logo" className="w-20 h-10" />
       <nav>
         <section className="MOBILE-MENU md:hidden">
           <div>
-            <button className="md:hidden mt-2 material-symbols-outlined">
-              menu
+            <button
+              className="md:hidden mt-2"
+              onClick={() => setNavOpen(!navOpen)}
+            >
+              {navOpen ? (
+                <span className="HAMBURGER-ICON absolute top-0 right-0 px-7 py-7 material-symbols-outlined">
+                  close
+                </span>
+              ) : (
+                <span className="CLOSE-ICON absolute top-0 right-0 px-7 py-7 material-symbols-outlined">
+                  menu
+                </span>
+              )}
             </button>
-          </div>
-          <div>
-            <span class="absolute top-0 right-0 px-7 py-7 material-symbols-outlined">
-              close
-            </span>
-          </div>
-          <div className="">
-            <ul className="Responsive-mobile-menu">
-              <li>
-                <a href="#" className="px-4 py-2 mr-4">
-                  About Afos
-                </a>
-              </li>
-              <li className="">
-                <a href="#" className="px-4 py-4 mr-4">
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a href="#" className="px-4 py-2 mr-4">
-                  Community
-                </a>
-              </li>
-              <button className="bg-yellow rounded-md px-6 py-4 font-semibold">
-                Submit Project
-              </button>
-            </ul>
+            <div
+              className={`${
+                navOpen ? "block" : "hidden"
+              }`}
+            >
+              <ul className="absolute mt-20  space-y-6 px-6 z-10 w-1/2 block top-0 right-0 h-screen bg-white Responsive-mobile-menu">
+                <li>
+                  <a href="#" className="underline hover:underline-offset-4">
+                    About Afos
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="underline hover:underline-offset-4">
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="underline hover:underline-offset-4">
+                    Community
+                  </a>
+                </li>
+                <button className="bg-yellow rounded-md px-6 py-4 font-semibold">
+                  Submit Project
+                </button>
+              </ul>
+            </div>
           </div>
         </section>
       </nav>
 
-      <div className="hidden md:flex list-none items-center">
+      <div className="DESKTOP-MENU hidden md:flex list-none items-center">
         <li>
           <a href="#" className="px-4 py-2 mr-4">
             About Afos
@@ -65,43 +75,6 @@ function Navbar() {
         </button>
       </div>
     </div>
-
-    // <div className=" w-[396px] md:w-full p-5 md:p-10">
-    //   <div className="flex justify-between">
-    //     <img src={logo} alt="logo" className="w-20 h-10" />
-
-    //     <nav>
-    //       <section className="mobile-menu">
-    //         <div
-    //           className="hamburger-icon"
-    //           onClick={() => setNavOpen((prev) => !prev)}
-    //         >
-    //           <button className="md:hidden material-symbols-outlined">menu</button>
-    //         </div>
-    //         <div className=""></div>
-
-    //         <div className="hidden md:block flex-col text-green text-base">
-    //           <button>
-    //             <a className="px-4 py-2 mr-4">About Afos</a>
-    //           </button>
-    //           <button>
-    //             <a href="#" className="px-4 py-2">
-    //               Projects
-    //             </a>
-    //           </button>
-    //           <button>
-    //             <a href="#" className="px-4 py-2">
-    //               Community
-    //             </a>
-    //           </button>
-    //           <button className="bg-yellow rounded-md px-9 py-4 font-semibold">
-    //             Submit Project
-    //           </button>
-    //         </div>
-    //       </section>
-    //     </nav>
-    //   </div>
-    // </div>
   );
 }
 

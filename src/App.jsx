5f4f4f644b784fbc8e0fc6f45ba9cd889aa736fe
Shubhankar <shubhankar.nautiyal21@gmail.com>
2023-08-08 22:23:22 +0530

    created projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Empowerment from "./components/Empowerment";
 import Enhancement from "./components/Enhancement";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Projects />
       <Empowerment />
       <Enhancement />
     </div>

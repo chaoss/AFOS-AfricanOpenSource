d8aad4f7f4e77d12273a82110de7837722d51ee0
Akintola Rahmat <akintolarahmah@gmail.com>
2023-08-09 16:30:53 +0000

    made style changes
    Signed-off-by: Akintola Rahmat <akintolarahmah@gmail.com>

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,14 +3,14 @@ import africamap_text from "../assets/images/africamap-text.svg";
 
 function Newsletter(){
     return(
-        <div className="bg-black w-full h-screen flex items-center justify-center flex-col space-y-8">
+        <div className="bg-black w-full h-full flex items-center justify-center flex-col space-y-8">
             {/* Africamap image */}
-             <div className="relative">
-             <img src={africamap} alt="africamap in lime-green color" className="w-96 h-120" />
+             <div className="relative w-2/5 h-150">
+             <img src={africamap} alt="africamap in lime-green color" className="w-auto" />
              <img 
              src={africamap_text} 
              alt="put your skill to work text as image" 
-             className="absolute top-1/2 left-35 transform  -translate-y-2/3" />
+             className="absolute top-24 left-35" />
              </div>
             
             {/* Newsletter form */}
@@ -19,17 +19,17 @@ function Newsletter(){
             
                 <form>
                     <div className="flex space-x-2">
-                        <div className="w-96 h-12 bg-white rounded-lg border border-yellow-100 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        <div className="w-96 h-12 bg-white rounded-lg border border-yellow-100 ">
                             <input
                             type="email"
                             name="email"
                             id="email"
                             autoComplete="email"
-                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-[#8A8A00] focus:ring-0 sm:text-sm sm:leading-6"
+                            className=" w-96 h-12 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-[#8A8A00] "
                             placeholder="Enter Email"
                             />
                         </div>
-                        <div className="text-center">
+                        <div className="text-center h-12">
                             <button type="submit" className=" bg-[#323200] hover:bg-slate-100 hover:text-[#323200] text-[#FFFFE3] text-base font-semibold leading-tight  p-3 px-10 rounded-lg z-10">Subscribe</button>
                             </div>
                         

effedc253b203f57a522b455bf824d00d9f5e573
Akintola Rahmat <akintolarahmah@gmail.com>
2023-08-18 06:26:56 +0000

    implemented mobile view designs
    Signed-off-by: Akintola Rahmat <akintolarahmah@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import Empowerment from "./components/Empowerment";
 import Enhancement from "./components/Enhancement";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
 
 function App() {
     return (
         <div>
             <Navbar />
+            <Projects />
             <Empowerment />
             <Enhancement />
             <Newsletter />
+            <Footer />
         </div>
     );
-
-
 }
 
 export default App;

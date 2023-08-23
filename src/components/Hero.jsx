2cfc65e3f5d04c610e9e3852ef36d8b2d3bd108e
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-08-23 15:13:56 +0300

    fixed mobile view on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroImage from "../assets/images/hero-image.png";
 
 function Hero() {
   return (
-    <div className="h-[60vh] md:h-full bg-gradient-to-r from-orange-50 to-lime-50">
+    <div className="h-[70vh] md:h-full bg-gradient-to-r from-orange-50 to-lime-50">
       <div className=" flex justify-center">
         <img src={heroImage} className="object-cover md:py-0" alt="map-vector" />
         <div className="absolute md:space-x-6 mt-[70%] md:mt-[28rem] flex flex-col md:block">

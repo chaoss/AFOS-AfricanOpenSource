36a02ea88a0db078ab2a775d9724bf07d5a9e041
ozaharsh95 <dsouzaaron77@gmail.com>
2024-09-02 21:08:54 +0530

    feat:Project Page with Pagination and Responsiveness
    Signed-off-by: Giftea <eleojogiftuhiene@gmail.com>

---
+++ src/pages/Project.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Projects from '../components/ProjectPage/Projects'
+import Footer from '../components/Footer'
+
+const ProjectPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Projects/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default ProjectPage

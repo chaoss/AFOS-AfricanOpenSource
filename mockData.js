675cf47d4c864147f1f548c4f774431d81f3bead
coolcat-coder <eleojogiftuhiene@gmail.com>
2024-09-13 17:03:35 +0100

    feat: Updated mock data with categories
    Signed-off-by: Giftea <eleojogiftuhiene@gmail.com>

--- mockData.js
+++ mockData.js
@@ -11,47 +11,123 @@ export const projectCardsData = [
     title: "Chakra UI",
     image: chakara,
     category: "UI Library",
-    description: "Chakra UI is a simple, modular and accessible component library that gives you the building blocks you need to build your React applications.",
-    project_link: "https://chakra-ui.com/"
+    description:
+      "Chakra UI is a simple, modular and accessible component library that gives you the building blocks you need to build your React applications.",
+    project_link: "https://chakra-ui.com/",
+    country: "Nigeria",
   },
   {
     id: "id-02",
     title: "CHAOSS Africa",
     image: chaoss,
     category: "Community",
-    description: "A chapter of the CHAOSS Project focusing on creating initiatives and metrics around solving challenges of Open Source Communities in Africa.",
-    project_link: "https://chaoss.community/"
+    description:
+      "A chapter of the CHAOSS Project focusing on creating initiatives and metrics around solving challenges of Open Source Communities in Africa.",
+    project_link: "https://chaoss.community/",
+    country: "Nigeria",
   },
   {
     id: "id-03",
     title: "OSCA - Open Source Community Africa",
     image: osca,
     category: "Community",
-    description: "A community of creative minds driving the Open Source movement in Africa.",
-    project_link: "https://oscafrica.org/"
+    description:
+      "A community of creative minds driving the Open Source movement in Africa.",
+    project_link: "https://oscafrica.org/",
+    country: "Nigeria",
   },
   {
     id: "id-04",
     title: "Igbo API",
     image: igboapi,
     category: "API",
-    description: "Access thousands of Igbo words, audio pronunciations, and example sentences to power the future of Igbo technology.",
-    project_link: "https://igboapi.com/"
+    description:
+      "Access thousands of Igbo words, audio pronunciations, and example sentences to power the future of Igbo technology.",
+    project_link: "https://igboapi.com/",
+    country: "Nigeria",
   },
   {
     id: "id-05",
     title: "SpaceYaTech",
     image: spaceyatech,
-    category: "Tech Community",
-    description: "SpaceYaTech is the fastest growing African Open-Source Community Looking To Change The Way Young Africans Get Started In Technology.",
-    project_link: "https://github.com/SpaceyaTech"
+    category: "Community",
+    description:
+      "SpaceYaTech is the fastest growing African Open-Source Community Looking To Change The Way Young Africans Get Started In Technology.",
+    project_link: "https://github.com/SpaceyaTech",
+    country: "Nigeria",
   },
   {
     id: "id-06",
     title: "Windi UI",
     image: windui,
     category: "UI Library",
-    description: "Beautiful and professionally crafted Windi UI components and templates.",
-    project_link: "https://github.com/selemondev/windi-ui"
-  }
+    description:
+      "Beautiful and professionally crafted Windi UI components and templates.",
+    project_link: "https://github.com/selemondev/windi-ui",
+    country: "Nigeria",
+  },
+  {
+    id: "id-07",
+    title: "Demo Project 1",
+    image:
+      "https://images.pexels.com/photos/319968/pexels-photo-319968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: "Robotics",
+    description: "Get Started In Robotics.",
+    project_link: "https://github.com/",
+    country: "Kenya",
+  },
+  {
+    id: "id-08",
+    title: "Demo Project 2",
+    image:
+      "https://images.pexels.com/photos/8849295/pexels-photo-8849295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: "Artificial Intelligence",
+    description: "Get Started In Artificial Intelligence.",
+    project_link: "https://github.com/",
+    country: "Ghana",
+  },
+  {
+    id: "id-09",
+    title: "Demo Project 3",
+    image:
+      "https://images.pexels.com/photos/1476321/pexels-photo-1476321.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: "Hardware",
+    description: "Get Started In Hardware.",
+    project_link: "https://github.com/",
+    country: "Kenya",
+  },
+  {
+    id: "id-10",
+    title: "Demo Project 4",
+    image:
+      "https://images.pexels.com/photos/26804169/pexels-photo-26804169/free-photo-of-white-dog-robot.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: "Artificial Intelligence",
+    description: "Get Started In Artificial Intelligence.",
+    project_link: "https://github.com/",
+    country: "Uganda",
+  },
+];
+
+export const categories = [
+  { category: "API", numOfProjects: 1 },
+  { category: "UI Library", numOfProjects: 2 },
+  { category: "Community", numOfProjects: 2 },
+  { category: "Hardware", numOfProjects: 0 },
+  { category: "Robotics", numOfProjects: 1 },
+  { category: "Artificial Intelligence", numOfProjects: 2 },
+  { category: "Hardware", numOfProjects: 1 },
+  { category: "Agriculture", numOfProjects: 0 },
+  { category: "eCommerce", numOfProjects: 0 },
+];
+
+export const countries = [
+  { country: "Ghana", numOfProjects: 0 },
+  { country: "Nigeria", numOfProjects: 5 },
+  { country: "Angola", numOfProjects: 0 },
+  { country: "Tanzania", numOfProjects: 0 },
+  { country: "Benin", numOfProjects: 0 },
+  { country: "Congo", numOfProjects: 0 },
+  { country: "Tunisia", numOfProjects: 0 },
+  { country: "Kenya", numOfProjects: 1 },
+  { country: "Uganda", numOfProjects: 1 },
 ];
